1bc5fd413e97e6ba60ea07423fa6ad4d320c4d7f
Doka7 <cobealex@gmail.com>
2016-03-13 12:06:33 +0100

    Actualización BBDD
    Se ha añadido un registro nuevo en varias tablas para agregar la opción
    predeterminada en los dropdown del formulario.

--- Descripcion y diseño/Interfaz/conectar.php
+++ Descripcion y diseño/Interfaz/conectar.php
@@ -22,3 +22,15 @@ catch(PDOException $e)
 $DBH = null;
 
 ?>
+
+
+// Imprimir los resultados en HTML
+echo "<table>\n";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
